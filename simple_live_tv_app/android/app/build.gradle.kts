cc3a2f67192ac8d6cc9c768d47ae365de60b16e3
kzlong <43065397+kzlong@users.noreply.github.com>
2026-01-31 09:09:12 +0800

    Update build.gradle.kts

--- simple_live_tv_app/android/app/build.gradle.kts
+++ simple_live_tv_app/android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.xycz.simple_live_tv"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
